f5819e28bbf8c71864f91e3326338d304339462e
Riya505 <riyapk505@gmail.com>
2022-10-19 22:09:56 +0530

    new

--- src/app/addstudent/addstudent.component.ts
+++ src/app/addstudent/addstudent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addstudent',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddstudentComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private myapi:ApiService) { }
   studentName=""
   rollNo=""
   admissionNo=""
@@ -36,6 +36,12 @@ export class AddstudentComponent implements OnInit {
       "guardianAddress":this.guardianAddress
     }
     console.log(data)
+    this.myapi.addStudent(data).subscribe(
+      (response)=>{
+        console.log(response)
+        alert("added")
+      }
+    )
   }
 
   ngOnInit(): void {
--- src/app/viewallstudents/viewallstudents.component.ts
+++ src/app/viewallstudents/viewallstudents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-viewallstudents',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewallstudentsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { 
+    this.fetchData()
+  }
+  fetchData=()=>{
+    this.myapi.viewStudents().subscribe(
+      (data)=>{
+        this.studentData=data
+      }
+    )
+  }
 
-  studentData=[{"name":"ann","rollNo":"1","admissionNo":"7656","collegeName":"GEC Idukki","department":"CS","bloodGroup":"A+","dob":"23/6/2000","parentName":"sam","parentEmail":"sam@gmail.com","guardianName":"daniel","guardianAddress":"daniel,ekm"},{"name":"zara","rollNo":"2","admissionNo":"7643","collegeName":"GEC palakkad","department":"EC","bloodGroup":"B+","dob":"2/7/2000","parentName":"david","parentEmail":"david@gmail.com","guardianName":"jhon","guardianAddress":"david,pkd"},{"name":"kripa","rollNo":"3","admissionNo":"8676","collegeName":"CET","department":"EEE","bloodGroup":"O-","dob":"4//121999","parentName":"alex","parentEmail":"alex@gmail.com","guardianName":"henry","guardianAddress":"abc,tvm"},{"name":"abi","rollNo":"4","admissionNo":"6783","collegeName":"TKM","department":"Civil","bloodGroup":"AB+","dob":"13/7/2000","parentName":"emma","parentEmail":"emma@gmail.com","guardianName":"james","guardianAddress":"klm"},{"name":"surya","rollNo":"5","admissionNo":"7232","collegeName":"CET","department":"ME","bloodGroup":"B-","dob":"5/2/1998","parentName":"joseph","parentEmail":"jo@gmail.com","guardianName":"william","guardianAddress":"tvm"}]
+  studentData:any=[]
 
   ngOnInit(): void {
   }
